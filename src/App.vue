e070eb9bb2d647728a4091b064c5146c392c5f0d
WLAjansen <40787982+WLAjansen@users.noreply.github.com>
2020-01-25 18:16:23 +0100

    Update 2
    Alles werkt

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,35 @@
 <template>
   <div id="app">
-    
+    <Songs v-bind:songs="songs" />
   </div>
 </template>
 
 <script>
+import Songs from './components/Songs';
+
 export default {
   name: 'app',
   components: {
-  
+    Songs
   },
   data() {
     return {
       songs: [
-        {
-          id: 1,
-          title: "Song one",
-          completed: false
-        },
-        {
-          id: 2,
-          title: "Song one",
-          completed: true
-        },
-        {
-          id: 3,
-          title: "Song one",
-          completed: false
-        },
+       {
+         id: 1,
+         title: "Song one",
+         completed: false
+       },
+       {
+         id: 2,
+         title: "Song two",
+         completed: true
+       },
+       {
+         id: 3,
+         title: "Song three",
+         completed: false
+       }
       ]
     }
   }
---
+++ src/components/Songs.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <div v-bind:key="song.id" v-for="song in songs">
+          <SongItem v-bind:song="song" />
+          </div>
+      </div>
+</template>
+
+<script>
+import SongItem from './SongItem.vue';
+
+export default {
+    name: "Songs",
+    components: {
+        SongItem
+    },
+    props: ["songs"]
+}
+</script>
+
+<style scoped>
+
+</style>
